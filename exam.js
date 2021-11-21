23c17b791dccd5f019b95f29306812b56362ed5f
mohammadmehedihasansabbir <86853457+mohammadmehedihasansabbir@users.noreply.github.com>
2021-11-21 20:14:28 +0600

    first commit

---
+++ exam.js
@@ -0,0 +1,12 @@
+function find_max(nums) {
+    let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
+    for (let num of nums) {
+        if (num > max_num) {
+            // (Fill in the missing line here)
+            max_num += num;
+        }
+    }
+    return max_num;
+}
+const valueExam = find_max(-10);
+console.log(valueExam);
